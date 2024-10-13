fe3da28ea7382d73feb12d657701c45fbe71fd7c
MedNourS <sabir.med.nour@gmail.com>
2024-10-13 19:41:42 -0400

    v0.2

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ def fileFinder() -> str:
         return None
 
 
+def main() -> bool:
+    user = input("What action would you like to execute?\n1. Add typos to your typo file\n2. Clear your typo file\n-> ")
+    if user == "1":
+        add(location)
+    elif user == "2":
+        clear(location)
+
+
 def add(file:str):
     changes = input("What changes would you like to make to the file? ").split(" ")
     with open(file, "a") as appender:
@@ -22,12 +30,21 @@ def clear(file:str):
     open(file, "w").close()
 
 
-def main():
-    location = fileFinder()
-    clear(location)
-    add(location)
+def engaged() -> bool:
+    user = input("Do you require another operation? ")
+    if user.lower() == "yes" or "y":
+        return True
+    else:
+        return False
 
 
 
 if __name__ == "__main__":
+    location = fileFinder()
     main()
+    while(True):
+        if engaged() == True:
+            print("\n")
+            main()
+        else:
+            break
